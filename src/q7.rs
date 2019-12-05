cc24ccb69bc45f4a8c78f63c2bc422c37e73ffa9
pfcoder <pfcoder97@gmail.com>
2019-12-05 21:52:16 +0800

    update

--- src/q7.rs
+++ src/q7.rs
@@ -1,5 +1,45 @@
 pub struct Solution {}
 
 impl Solution {
-  pub fn reverse(x: i32) -> i32 {}
+  pub fn reverse(x: i32) -> i32 {
+    let mut p = x as i64;
+    let mut result: i64 = 0;
+    let mut count = 0;
+    let base: i64 = 2;
+
+    let upper_bound: i64 = base.pow(31) - 1;
+    let lower_bound: i64 = -base.pow(31);
+
+    loop {
+      if p == 0 {
+        return result as i32;
+      }
+      let m = p % 10;
+      p = p / 10;
+
+      result = result * 10 + m;
+
+      if result > upper_bound || result < lower_bound {
+        return 0;
+      }
+
+      count += 1;
+    }
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn test_7() {
+    assert_eq!(Solution::reverse(123), 321);
+    assert_eq!(Solution::reverse(-123), -321);
+    assert_eq!(Solution::reverse(0), 0);
+    assert_eq!(Solution::reverse(-123000), -321);
+    let base: i64 = 2;
+    assert_eq!(Solution::reverse((base.pow(31) - 1) as i32), 0);
+    assert_eq!(Solution::reverse((-base.pow(31)) as i32), 0);
+  }
 }
